0f680f8130353272da117a7ed957fe40605afc11
Alexander Kämmerer <a.kaemmerer@factor-eleven.de>
2021-04-20 08:37:32 +0200

    fix: fixed eslint violations

--- packages/libs/navigation/src/types.tsx
+++ packages/libs/navigation/src/types.tsx
@@ -1,8 +1,8 @@
-import {RedirectProps, RouteProps} from "react-router";
+import { RedirectProps, RouteProps } from "react-router";
 
 export interface Route<TId extends string>
     extends Omit<RouteProps, "render" | "location">,
-        Partial<Pick<RedirectProps, "to" | "from" | "push">> {
+    Partial<Pick<RedirectProps, "to" | "from" | "push">> {
     readonly id: TId;
     readonly roles?: string[];
     readonly routes?: ReadonlyArray<Route<TId>>;
