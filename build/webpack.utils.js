85e0581c5f11e9d50590954db39ba450c584b09d
Alexander Kämmerer <a.kaemmerer@factor-eleven.de>
2021-04-21 18:01:56 +0200

    feat: added root eslint execution

--- build/webpack.utils.js
+++ build/webpack.utils.js
@@ -3,40 +3,40 @@ const DEVELOPMENT_ENV = "development";
 const PRODUCTION_ENV = "production";
 const TESTING_ENV = "testing";
 
-const VALID_ENVIRONMENTS = [DEVELOPMENT_ENV, PRODUCTION_ENV, TESTING_ENV];
+const VALID_ENVIRONMENTS = [ DEVELOPMENT_ENV, PRODUCTION_ENV, TESTING_ENV ];
 
 module.exports = {
-    DEVELOPMENT_ENV,
-    PRODUCTION_ENV,
+  DEVELOPMENT_ENV,
+  PRODUCTION_ENV,
 
-    /**
+  /**
      * return the current setted NODE_ENV value
      * @returns {string}
      */
-    getEnvironment: () => ENVIRONMENT,
+  getEnvironment: () => ENVIRONMENT,
 
-    /**
+  /**
      * Validates if the setted environment has a valid and supported value
      * @returns {boolean}
      */
-    isValidEnvironment: () => VALID_ENVIRONMENTS.includes(ENVIRONMENT),
+  isValidEnvironment: () => VALID_ENVIRONMENTS.includes( ENVIRONMENT ),
 
-    /**
+  /**
      * returns an array of valid values for the NODE_ENV
      * @returns {[string, string, string]}
      */
-    getValidEnvironments: () => VALID_ENVIRONMENTS,
+  getValidEnvironments: () => VALID_ENVIRONMENTS,
 
-    /**
+  /**
      * return if a dev build is running
      * @returns {boolean}
      */
-    isDevelopmentBuild: () => ENVIRONMENT === DEVELOPMENT_ENV,
+  isDevelopmentBuild: () => ENVIRONMENT === DEVELOPMENT_ENV,
 
-    /**
+  /**
      * return if a prod build is running
      * @returns {boolean}
      */
-    isProductionBuild: () => ENVIRONMENT === PRODUCTION_ENV,
+  isProductionBuild: () => ENVIRONMENT === PRODUCTION_ENV,
 
 };
