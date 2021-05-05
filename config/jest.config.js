559665d592d71ec18abb9a5fad628e1be83dd7e3
Alexander Kämmerer <a.kaemmerer@factor-eleven.de>
2021-05-05 09:00:17 +0200

    refactor: moved __mocks__ folder

--- config/jest.config.js
+++ config/jest.config.js
@@ -27,8 +27,8 @@ module.exports = packagePath => ( {
     "json",
   ],
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/staticfiles.mock.js",
-    "\\.(css|less|scss)$": "<rootDir>/src/__mocks__/scss.mock.js",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/staticfiles.mock.js",
+    "\\.(css|less|scss)$": "<rootDir>/__mocks__/scss.mock.js",
     "@components/(.*)": "<rootDir>/src/components/$1",
     "@containers/(.*)": "<rootDir>/src/containers/$1",
     "@pages/(.*)": "<rootDir>/src/pages/$1",
@@ -37,7 +37,6 @@ module.exports = packagePath => ( {
     "@store/(.*)": "<rootDir>/src/store/$1",
     "@res/(.*)": "<rootDir>/res/$1",
     "@mocks/(.*)": "<rootDir>/__mocks__/$1",
-    "@tests/(.*)": "<rootDir>/__tests__/$1",
   },
   globals: {
     __ENVIRONMENT__: "production",
